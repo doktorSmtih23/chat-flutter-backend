923c85bec86eef071dd10f9271549b4e9715f846
Christian Ordonez <csordonez@hotmail.com>
2020-11-10 20:11:21 -0500

    Backend terminado

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
-//DB config
+// DB Config
+require('./database/config').dbConnection();
 
-const { dbConnection } = require('./database/config').dbConnection();
 
 // App de Express
 const app = express();
 
-//Lectura y parseo del Body
+// Lectura y parseo del Body
 app.use(express.json());
+
 
 // Node Server
 const server = require('http').createServer(app);
@@ -24,11 +25,12 @@ require('./sockets/socket');
 const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
 
-//Mis rutas
 
+
+// Mis Rutas
 app.use('/api/login', require('./routes/auth'));
-
-
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/mensajes', require('./routes/mensajes'));
 
 
 
